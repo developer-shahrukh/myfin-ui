51a4ac1475cf2084d2a0be7718da6fdacc39b76e
developer-shahrukh <shahrukh4854@gmail.com>
2025-02-16 20:43:34 +0530

    Initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,21 @@
-import { AppBar, Box, Container, Input, InputLabel, Menu, MenuItem, Toolbar, Typography } from "@mui/material"
-import { Black_And_White_Picture } from "next/font/google"
+import { AppBar, Container, MenuItem, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
 
 
-const Navbar=()=> {
-  return (
-    <AppBar position="static">
-        <Container>
-            <Toolbar >
-            
-               <MenuItem>Home</MenuItem>
-               <MenuItem>User</MenuItem>
-               <MenuItem>Account</MenuItem>
-               <MenuItem>Category</MenuItem>
-            </Toolbar>
-        </Container>
-    </AppBar>
-  )
+
+const Navbar=()=>{
+
+  return(
+   <AppBar >
+      <Container >
+        <Toolbar >
+          
+        </Toolbar>
+      </Container>
+   </AppBar>
+  );
 }
 
-export default Navbar
+
+
+export default Navbar;
