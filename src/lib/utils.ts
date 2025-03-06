85e851484e1576aa6ab019d166ee18979967ed49
developer-shahrukh <shahrukh4854@gmail.com>
2025-03-06 22:21:58 +0530

    MF-6 implement more API's

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,65 +1,115 @@
-const baseURL=process.env.NEXT_PUBLIC_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
-const getUsers=()=>{
-    var promise=new Promise((resolve,reject)=>{
-        fetch(`${baseURL}/users/getUsers`).then((response)=>{
-            return response.json();
-        }).then((users)=>{
-            resolve(users);
-        }).catch((error)=>{
-            reject(error);
-        });
-    });
-}
-
-const getUserByCode=()=>{
-    var userCode=0;
-    var promise=new Promise((resolve,reject)=>{
-        fetch(`${baseURL}/users/getUser${userCode}`).then((response)=>{
-            return response.json();
-        }).then((user)=>{
-            resolve(user);
-        }).catch((error)=>{
-            reject(error);
-        });
-    });
-}
-
-const getAccounts=()=>{
-    var promise=new Promise((resolve,reject)=>{
-        fetch(`${baseURL}/accounts/getAccounts`).then((response)=>{
-            return response.json();
-        }).then((accounts)=>{
-            resolve(accounts);
-        }).catch((error)=>{
-            reject(error);
-        });
-    });
-}
-
-
-const getAccountByCode=()=>{
-    var accountCode=0;
-    var promise=new Promise((resolve,reject)=>{
-        fetch(`${baseURL}/accounts/getAccount${accountCode}`).then((response)=>{
-            return response.json();
-        }).then((account)=>{
-            resolve(account);
-        }).catch((error)=>{
-            reject(error);
-        });
-    });
-}
-
-const getLoan=()=>{
-    var promise=new Promise((resolve,reject)=>{
-    fetch(`${baseURL}/loans/getLoan`).then((response)=>{
-        return response.json()
-    }).then((loan)=>{
-        resolve(loan);
-    }).catch((error)=> {
+const getUsers = () => {
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/users/getUsers`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        resolve(users);
+      })
+      .catch((error) => {
         reject(error);
-    });
-});
-}
+      });
+  });
+  return promise;
+};
 
+const getUserByCode = () => {
+  var userCode = 0;
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/users/getUser${userCode}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((user) => {
+        resolve(user);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return promise;
+};
+
+const getAccounts = () => {
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/accounts/getAccounts`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((accounts) => {
+        resolve(accounts);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return promise;
+};
+
+const getAccountByCode = () => {
+  var accountCode = 0;
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/accounts/getAccount${accountCode}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((account) => {
+        resolve(account);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return promise;
+};
+
+const getLoans = () => {
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/loans/getLoans`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((loan) => {
+        resolve(loan);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return promise;
+};
+
+const getLoanByCode = (loanId) => {
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/loans/getLoan${loanId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((loan) => {
+        resolve(loan);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return promise;
+};
+
+const getTransaction = () => {
+  var promise = new Promise((resolve, reject) => {
+    fetch(`${baseURL}/transactions/getTransactions`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((loan) => {
+        resolve(loan);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return promise;
+};
